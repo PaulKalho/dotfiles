a6a19dcf725519bf911332fe628172de58a4711e
Paul Kalhorn <kalhornpaul@gmail.com>
2026-03-25 14:38:16 +0100

    feat: add tmux, alacritty, zsh to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.stateVersion = "23.11";
@@ -99,4 +99,161 @@
     ruff
     nodePackages.markdownlint-cli
   ];
+
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "philips";
+      plugins = [
+        "git"
+      ];
+    };
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      dc = "docker compose";
+      dp = "docker ps";
+      c = "clear";
+      dot = "cd ~/.config/dotfiles";
+      pp = "pwd";
+      ga = "git add";
+      gc = "git commit";
+      gd = "git diff";
+      gp = "git push";
+      gch = "git checkout";
+    };
+
+    initContent = ''
+      # start tmux automatically (safe version)
+      if command -v tmux >/dev/null && [ -z "$TMUX" ] && [ "$TERM" != "dumb" ]; then
+        tmux new-session -A -s main
+      fi
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      env = {
+        TERM = "xterm-256color";
+      };
+
+      font.normal.family = "JetBrains Mono NF";
+
+      window = {
+        decorations = "none";
+        dimensions = {
+          columns = 90;
+          lines = 30;
+        };
+      };
+
+      colors = {
+        primary = {
+          background = "0x1b182c";
+          foreground = "0xcbe3e7";
+        };
+
+        normal = {
+          black = "0x100e23";
+          red = "0xff8080";
+          green = "0x95ffa4";
+          yellow = "0xffe9aa";
+          blue = "0x91ddff";
+          magenta = "0xc991e1";
+          cyan = "0xaaffe4";
+          white = "0xcbe3e7";
+        };
+
+        bright = {
+          black = "0x565575";
+          red = "0xff5458";
+          green = "0x62d196";
+          yellow = "0xffb378";
+          blue = "0x65b2ff";
+          magenta = "0x906cff";
+          cyan = "0x63f2f1";
+          white = "0xa6b3cc";
+        };
+      };
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+
+    plugins = [
+      {
+        plugin = pkgs.tmuxPlugins.minimal-tmux-status;
+
+        extraConfig = ''
+          set -g @minimal-tmux-fg "#000000"
+          set -g @minimal-tmux-bg "#698DDA"
+          set -g @minimal-tmux-justify "centre"
+          set -g @minimal-tmux-indicator-str "  tmux  "
+          set -g @minimal-tmux-indicator true
+          set -g @minimal-tmux-status "top"
+
+          # Enables or disables the left and right status bar
+          set -g @minimal-tmux-right true
+          set -g @minimal-tmux-left true
+
+          # expanded icon (fullscreen icon)
+          set -g @minimal-tmux-expanded-icon "󰊓 "
+
+          # on all tabs (default is false)
+          # false will make it visible for the current tab only
+          set -g @minimal-tmux-show-expanded-icon-for-all-tabs true
+
+          # To add or remove extra text in status bar
+          set -g @minimal-tmux-status-right-extra ""
+          set -g @minimal-tmux-status-left-extra ""
+
+          # To make the selection box rounded () or edged <>
+          # Default is nothing, when set to true default is edged
+          set -g @minimal-tmux-use-arrow true
+          set -g @minimal-tmux-right-arrow ""
+          set -g @minimal-tmux-left-arrow ""
+
+          # Not recommended to change these values
+          set -g @minimal-tmux-status-right "#S"
+          set -g @minimal-tmux-status-left "refer to code"
+
+          # If getting strings cut in left status or right
+          # Here 20 is the length of the characters in the string
+          set -g status-right-length 20
+          set -g status-left-length 20        
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      # general behavior
+      set-option -g history-limit 25000
+      set -g mouse on
+
+      set -g status-position top
+      set -g status-interval 1
+      set -g status-left ""
+
+      # indexing
+      set -g base-index 1
+      setw -g pane-base-index 1
+
+      # reload config
+      bind-key -r r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+
+      # splits keep cwd
+      bind '"' split-window -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
+
+      set -g default-terminal "screen-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+    '';
+  };
 }
